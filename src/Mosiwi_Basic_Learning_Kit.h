a67771adc927ad7864bd8d3d99780906d512e91d
mosiwi <354568001@qq.com>
2022-06-26 13:17:00 +0800

    update

---
+++ src/Mosiwi_Basic_Learning_Kit.h
@@ -0,0 +1,17 @@
+/*
+ This example works with the Mosiwi Basic learning board.
+ 
+ function: Drive led through 74HC595
+ Web: http://mosiwi.com/
+ Wiki: http://wiki.mosiwi.com/
+ Designer: jalen
+ Date：2022-6-22
+ */
+#ifndef Mosiwi_Basic_Learning_Kit_h
+#define Mosiwi_Basic_Learning_Kit_h
+#include <Arduino.h>
+
+# Don't use.
+
+
+#endif
